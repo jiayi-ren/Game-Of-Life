d8c7fad73294a081ffe51f4c4692a10dbe920300
Jiayi Ren <jiayiren22@gmail.com>
2020-07-27 20:07:48 -0700

    project set up

---
+++ src/Game.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Game = props => {
+
+    const [speed, setSpeed] = useState(100); // speed, 1 generation/100ms
+    const [rows, setRows] = useState(50); // row
+    const [cols, setCols] = useState(50);
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default Game;
