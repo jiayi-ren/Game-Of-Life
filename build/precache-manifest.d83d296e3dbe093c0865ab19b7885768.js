c75531922211bd568c618727af1d99bea53ed7ce
Jiayi Ren <jiayiren22@gmail.com>
2020-07-29 18:56:49 -0700

    build

---
+++ build/precache-manifest.d83d296e3dbe093c0865ab19b7885768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84dc49e83070e15e19e59d8568e77b5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c80efea14d23a67c20ae",
+    "url": "/static/css/main.5664e08c.chunk.css"
+  },
+  {
+    "revision": "189c8a3e8f92e3d2c717",
+    "url": "/static/js/2.c6da9aa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6da9aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80efea14d23a67c20ae",
+    "url": "/static/js/main.acf3c730.chunk.js"
+  },
+  {
+    "revision": "a5ea60d6c88a0d34a412",
+    "url": "/static/js/runtime-main.466ccbfb.js"
+  }
+]);
